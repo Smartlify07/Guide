f6a97ce23b5227732cfac2068c8a3112b080c7f7
Obinna Anosike <smartlify09@gmail.com>
2025-10-24 19:46:25 +0100

    build submission section

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from '@/components/features/landingpage/hero-section';
 import ServicesSection from '@/components/features/landingpage/services-section';
+import SubmissionSection from '@/components/features/landingpage/submission-section';
 import SupportSection from '@/components/features/landingpage/support-section';
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <SupportSection />
       <ServicesSection />
+      <SubmissionSection />
     </>
   );
 }
