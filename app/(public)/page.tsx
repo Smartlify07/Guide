02cac7a91d24aa6b8032ca635998ee880074ef70
Obinna Anosike <smartlify09@gmail.com>
2025-10-24 19:15:04 +0100

    build support section

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from '@/components/features/landingpage/hero-section';
+import SupportSection from '@/components/features/landingpage/support-section';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <SupportSection />
     </>
   );
 }
